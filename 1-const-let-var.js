69f1b674cdbe124e4dd0b70c09c7352b99aa05c8
Gaidmanee <gaidmanee@MacBook-Air-khxng-Gaidmanee.local>
2019-09-21 12:18:02 +0700

    add javaSC

---
+++ 1-const-let-var.js
@@ -0,0 +1,6 @@
+var a = "a";
+{
+  var a = "b";
+  console.log(1, a);
+}
+console.log(2, a);
---
+++ 2-if-for.js
@@ -0,0 +1,45 @@
+// const a = "a";
+// if (a === "a") {
+//   console.log("It's a");
+// } else {
+//   console.log("It's not a");
+// }
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+// console.log(i);
+
+// let i = 0;
+// while (i < 10) {
+//   console.log(i);
+//   i++;
+// }
+
+// let i = 0;
+// let sum = 0;
+// while (i <= 1000) {
+//   sum = sum + i;
+//   console.log(sum);
+//   i++;
+// }
+
+// let a = 1; // int a = 1
+// let b = 1.1; // float b = 1.1
+// let c = "string"; // string c = "hello"
+// let d = true; // bool a = true
+// let e = false; // bool e = false
+// let f = ['a' ,'b','d']; // string *f = [ " a " , " b " , " c "]
+// f[o] = "f";
+// f.push("h");
+// console.log(f);
+// let h = [];
+
+let student = {
+  id: "60110673",
+  name: "Gaidmanee",
+  faculty: "Science"
+};
+console.log(student.name);
+student.name = "Tongbai";
+console.log(student.name);
